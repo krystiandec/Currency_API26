d30e12da3c8529938d46a06e7a95b19c7362b6ae
Krystian <49121182+krystiandec@users.noreply.github.com>
2020-12-13 14:42:47 +0100

    dodanie menu wyglądu graficznego oraz jest tworzenie i przełączanie pomiedzy opcjami w MainActivity

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.currency;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,17 +26,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mConvertedTextView = (TextView)findViewById(R.id.txt_converted);
-        mAmountEditText = (EditText)findViewById(R.id.edt_amount);
-        mCalcButton = (Button)findViewById(R.id.btn_calc);
-        mForSpinner = (Spinner)findViewById(R.id.spn_for);
-        mHomSpinner= (Spinner)findViewById(R.id.spn_hom);
+        mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
+        mAmountEditText = (EditText) findViewById(R.id.edt_amount);
+        mCalcButton = (Button) findViewById(R.id.btn_calc);
+        mForSpinner = (Spinner) findViewById(R.id.spn_for);
+        mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
 
         ArrayList<String> arrayList = ((ArrayList<String>)
                 getIntent().getSerializableExtra(SplashActivity.KEY_ARRAYLIST));
         Collections.sort(arrayList);
         mCurrencies = arrayList.toArray(new String[arrayList.size()]);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.mnu_codes: {
+                // TODO: 13.12.2020 zdefiniować działanie
+            }break;
+            case R.id.mnu_invert: {
+                // TODO: 13.12.2020 zdefiniować działanie
+            }break;
+            case R.id.mnu_exit: {
+                finish();
+            }break;
+        }
+
+        return true;
     }
 }
