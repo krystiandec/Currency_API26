e059d43494a964833dc31089cc8f5eac576538d8
krystiandec <49121182+krystiandec@users.noreply.github.com>
2020-12-17 10:29:20 +0100

    pobranie klucza i zdefiniowanie składowych i stałych.

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -19,8 +20,12 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Button mCalcButton;
@@ -31,6 +36,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public static final String FOR = "FOR_CURRENCY";
     public static final String HOM = "HOM_CURRENCY";
+
+    private String mKey;
+
+    public static final String RATES = "rates";
+    public static final String URL_BASE =
+            "https://openexchangerates.org/api/latest.json?app_id=";
+    private static final DecimalFormat DECIMAL_FORMAT = new
+            DecimalFormat("#,##0.00000");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 //obsługa zachowania
             }
         });
+        mKey = getKey("open+key");
     }
 
     @Override
@@ -179,6 +193,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private String extractCodeFromCurrency(String currency) {
         return (currency).substring(0, 3);
+    }
+
+    private String getKey(String keyName){
+        AssetManager assetManager = this.getResources().getAssets();
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = assetManager.open("keys.properties");
+            properties.load(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  properties.getProperty(keyName);
+
     }
 
 }
