e222407a6d73453c66759a11259ee03ee9289a01
krystiandec <49121182+krystiandec@users.noreply.github.com>
2020-12-18 13:09:28 +0100

    nauczyć się testować w JUnit + obczaić bibliotekę Androidx: https://developer.android.com/reference/android/support/test/rule/ActivityTestRule

---
+++ app/src/androidTest/java/com/example/currency/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.currency;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class MainActivityTest extends TestSetup {
+
+    public MainActivityTest(Test test) {
+        super(test);
+    }
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        fTest.
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
